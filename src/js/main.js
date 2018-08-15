f0f7d377bfe83338e3468a14c5ecc3c8d910b60b
antonella <antonella_caroca@hotmail.com>
2018-08-15 14:35:39 -0300

    se puede buscar imagenes en general

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,16 @@ let searchPin = document.getElementById('searchPin');
 searchPin='cats';
 let images = document.getElementById('images');
 
+
+
+images.innerHTML = `<div class='pixabay_widget' data-search=${searchPin} data-max-rows='3' data-row-height='180' data-branding='false' target:'_blank' data-navpos='top'></div>`;
+
+function buscar(){
+searchPin = document.getElementById('searchPin').value;
 images.innerHTML = `<div class='pixabay_widget' data-search=${searchPin} data-max-rows='3' data-row-height='180' data-branding='false' data-navpos='top'></div>`;
+console.log(searchPin);
+new initPixabayWidget();
 
-
+}
 
  
